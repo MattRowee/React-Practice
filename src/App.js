ec4c9f01ea20b1c82edb2c98639d8978e5d64655
Matthew Rowe <matthew.rowe@ibm.com>
2021-02-23 21:33:35 -0500

    Styled Components library, dynamically styling components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import styled from 'styled-components';
 
+const StyledButton = styled.button`
+      background-color: ${props => props.alt ? 'red':'green'};
+      font: inherit;
+      border: 1px solid blue;
+      padding: 8px;
+      cursor: pointer;
+      border-radius: 3px;
+      
+      &:hover {
+        background: lightgreen;
+        color: black
+`
 class App extends Component {
   state = {
     persons: [
@@ -68,14 +81,18 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-      borderRadius: "3px"
-    };
+    // const style = {
+    //   backgroundColor: "white",
+    //   font: "inherit",
+    //   border: "1px solid blue",
+    //   padding: "8px",
+    //   cursor: "pointer",
+    //   borderRadius: "3px",
+    //   ":hover": {
+    //     background: 'lightgreen',
+    //     color: 'black',
+    //   }
+    // };
 
     let persons = null;
     // This was originally in the JSX as a ? terniary statement, but 
@@ -95,17 +112,32 @@ if(this.state.showPersons){
           />
       })}
         </div> 
-  )
-}
+    );
+    // style.backgroundColor = "red";
+    // style[':hover'] ={
+    //   background: 'salmon',
+    //   color: 'black',
+    // }
+    // style.border = "1px solid silver"; 
+    // style.color = "aliceblue";
+    // style.fontWeight = "bold";
+    }
 
+    let classes = [];
+     if(this.state.persons.length <= 2){
+       classes.push('red');
+     }
+     if(this.state.persons.length <= 1) {
+       classes.push('bold');
+     }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button 
-          className="primeButton"
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle List</button>
+        <p className={classes.join(" ")}>This is really working!</p>
+        <StyledButton 
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}>
+            Toggle List</StyledButton>
           {/* terniary expression. a property in state such as showPersons 
           can become an if statement that defaults to null with 2 lines of code */}
         {persons}
@@ -117,67 +149,4 @@ if(this.state.showPersons){
 
 export default App;
 
-
-// import React, { useState } from 'react';
-// import './App.css';
-// import Person from './Person/Person'
-
-// const app = props => {
-//   const [ personState, setPersonsState] = useState({
-//     persons: [
-//       {name: 'Max', age: 28},
-//       {name: 'Manu', age: 29},
-//       {name: 'Mark', age: 26}
-//     ],
-//   });
-    
-// const [otherState, setOtherState] = useState("Some other value");
-
-//   console.log(personState, otherState);
-
-//       // Naming convention 'handler' because it is a 
-//       // Method you are not actively calling
-
-//   const swtichNameHandler =()=>{
-
-//       // console.log("This is switchhandler (click)in App.js")
-//       // DONT DO THIS: this.state.persons[0].name = "Matthew";
-//       // Props and changes to state are the only thing 
-//       // that cause React to update the Dom
-//       // Componenet React will update state, but leave unspecified data alone!
-//       // functional react components are not the same: they replace 
-//       // state, they dont merge!!!!
-
-//       setPersonsState({
-//         persons: [
-//           { name: 'Matthew', age: 35},
-//           { name: 'Nina', age: 10},
-//           { name: 'Seth', age: 38}
-//         ]
-//       });
-//     };
-// // functional React components dont need render() methods
-
-//     return (
-//       <div className="App container">
-//      <h1>Hi Matt.</h1>
-//      <p>You are truly a good guy</p>
-//      <button onClick={swtichNameHandler}>switch name</button>
-//      <Person 
-//         name={personState.persons[0].name} 
-//         age={personState.persons[0].age}/>
-//      <Person 
-//         name={personState.persons[1].name}
-//         age={personState.persons[1].age}
-//         click={this.swtichNameHandler} 
-//         >My Hobbies: Racing
-//         </Person>
-//      <Person 
-//         name={personState.persons[2].name} 
-//         age={personState.persons[2].age}/>
-//       </div>
-//     );
-//     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work???'))
-//   }
-
-// export default app;
+  
